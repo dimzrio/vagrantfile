a2f5c49242cbafdc142a04fb44b4dd0f80e6f915
dimas.riotantowi <dimas.riotantowi@detik.com>
2018-12-16 11:32:35 +0700

    add vagrant ipfs

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 Example of Vagrantfile for describe the type of machine required for a project and how to configure or provision these machine.
 
-<h6> Installation </h6>
+<h3> Installation </h3>
 
 Most of vagrantfile example on this repo using engine virtualbox.
 Please install virtualbox before run these script.
 
+~~~~
 $ vagrant up
+~~~~
---
+++ ipfs/provisioning.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+sudo yum -y install nano wget net-tools
+sudo wget https://dist.ipfs.io/go-ipfs/v0.4.17/go-ipfs_v0.4.17_linux-amd64.tar.gz -P /root/
+sudo tar -zxvf /root/go-ipfs_v0.4.17_linux-amd64.tar.gz -C /root/
+sudo cp /root/go-ipfs/ipfs /bin/ipfs
+sudo /bin/ipfs init
+sudo cat << EOF > /usr/lib/systemd/system/ipfsd.service
+[Unit]
+Description=ipfs daemon
+
+[Service]
+ExecStart=/bin/ipfs daemon
+Restart=always
+User=root
+Group=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl enable ipfsd
+sudo systemctl restart ipfsd
