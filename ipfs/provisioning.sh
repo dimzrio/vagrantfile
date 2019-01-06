7ac9ddad33544e18953c0570d837a158ea1148a9
dimzrio <driotantowi@gmail.com>
2019-01-06 17:27:18 +0700

    change version

--- ipfs/provisioning.sh
+++ ipfs/provisioning.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum -y install nano wget net-tools
-sudo wget https://dist.ipfs.io/go-ipfs/v0.4.17/go-ipfs_v0.4.17_linux-amd64.tar.gz -P /root/
-sudo tar -zxvf /root/go-ipfs_v0.4.17_linux-amd64.tar.gz -C /root/
+sudo wget https://dist.ipfs.io/go-ipfs/v0.4.18/go-ipfs_v0.4.18_linux-amd64.tar.gz -P /root/
+sudo tar -zxvf /root/go-ipfs_v0.4.18_linux-amd64.tar.gz -C /root/
 sudo cp /root/go-ipfs/ipfs /bin/ipfs
 sudo /bin/ipfs init
 sudo cat << EOF > /usr/lib/systemd/system/ipfsd.service
